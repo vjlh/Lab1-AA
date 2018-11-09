b0ac20ad553b9f99e887ec35a1dc9729bd11b505
Valentina Ligueno <valentina.ligueno@gmail.com>
2018-11-09 01:40:29 -0300

    ksii

--- proba.c
+++ proba.c
@@ -6,13 +6,18 @@
 int CONT;
 int TAM_LISTA;
 
-int* insertarNumero(int lista[],int numero,int posicion, int tam)
+char* insertarNumero(char* lista,char numero,int posicion)
 {
-	int *listaInsertada;
-	listaInsertada = (int*)malloc(sizeof(int)*(tam+1));
+	char *listaInsertada;
+	//char str;
+	int largoArray = strlen(lista);
+	listaInsertada = (char*)malloc(sizeof(char)*(largoArray+1));
+
+	//sprintf(&str, "%d", numero);
 	listaInsertada[posicion] = numero;
+
 	int j = 0;
-	for (int i = 0; i < tam; ++i)
+	for (int i = 0; i < largoArray; ++i)
 	{
 		if(i == posicion){
 			j++;
@@ -22,9 +27,9 @@ int* insertarNumero(int lista[],int numero,int posicion, int tam)
 			listaInsertada[j] = lista[i];
 		j++;
 	}
-	/*for (int i = 0; i < tam+1; ++i)
+	/*for (int i = 0; i < largoArray+1; ++i)
 	{
-		printf("%i - ",listaInsertada[i]);
+		printf("%c - ",listaInsertada[i]);
 	}*/
 	return listaInsertada;
 }
@@ -32,31 +37,27 @@ int* insertarNumero(int lista[],int numero,int posicion, int tam)
 //def inserta_multiple(x, lst):
   //  return [inserta(x, lst, i) for i in range(len(lst) + 1)]
 
-int** insertarMultiple(int numero, int lista[], int tamLista)
+char** insertarMultiple(char* lista,char numero)
 {
-	int **listaDevuelta;
-	listaDevuelta = (int**)malloc(sizeof(int*)*(tamLista+1));
-	for (int i = 0; i < tamLista+1; ++i)
+	char **listaDevuelta;
+	int largoArray = strlen(lista);
+	listaDevuelta = (char**)malloc(sizeof(char*)*(largoArray+1));
+	for (int i = 0; i < largoArray+1; ++i)
 	{
-		listaDevuelta[i] = (int*)malloc(sizeof(int)*(tamLista+1));
+		listaDevuelta[i] = (char*)malloc(sizeof(char)*(largoArray+1));
 	}
 
 	//int listaDevuelta[tamLista+1][tamLista+1];
-	for (int i = 0; i < tamLista+1; ++i)
+	for (int i = 0; i < largoArray+1; ++i)
 	{
-		listaDevuelta[i] = insertarNumero(lista,numero,i,tamLista);
+		listaDevuelta[i] = insertarNumero(lista,numero,i);
 	}
-	return listaDevuelta;
-	for (int i = 0; i < tamLista+1; ++i)
+	for (int i = 0; i < largoArray+1; ++i)
 	{
-		printf("[");
-		for (int j = 0; j < tamLista+1; ++j)
-		{
-			printf("%i,",listaDevuelta[i][j]);		
-		}
-		printf("]\n");
+		printf("[%s]\n",listaDevuelta[i]);		
 
 	}
+	return listaDevuelta;
 
 }
 
@@ -66,43 +67,28 @@ int** insertarMultiple(int numero, int lista[], int tamLista)
     return sum([inserta_multiple(c[0], s)
                 for s in permuta(c[1:])],[])*/
 
-int** permutaciones(int lista[])
+char** permutaciones(char *lista)
 {
-	CONT++;
-	if (CONT == TAM_LISTA)
+	if (strlen(lista) == 0)
 	{
-		int lista[] = {}
-		return lista
+		return "";
 	}
 	else
-		
 		int insertaMul[][] = insertaMultiple(lista[0],s)
 		return 
 
 }
 
-int lenLista(int* lista)
-{
-	int tamano_lista = sizeof(lista);
-	printf("TAML: %d\n",tamano_lista);
-    int tamano_entero = sizeof(int);
-	printf("TAME: %d\n",tamano_entero);
-    int cantidad_elem = tamano_lista/tamano_entero;
-    return cantidad_elem;
-
-}
-
 int main(int argc, char const *argv[])
 {
-	//int* xd;
-	//xd = (int*)malloc(sizeof(int)*6);
-	int xd[] = {};
-	TAM_LISTA = 3;
-	int tamano_lista = sizeof(xd);
-	printf("TAML: %d\n",tamano_lista);
+	char* xd;
+	xd = (char*)malloc(sizeof(char)*3);
+	strcpy(xd,"124");
+	//char xd[3] = {'1','2','4'};
+	//printf("TAMA: %lu\n",strlen(xd));
 
+	insertarMultiple(xd,'m');
 
-    printf("La cantidad de elem es: %d\n",lenLista(xd));
 
 	//int tamano; ///Aplica sizeof al array
 	//int xd[8] = {1,2,3,4,5,6,7,8};
