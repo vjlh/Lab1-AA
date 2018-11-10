b1a3a515cda6b8de73c3028be5624f4666ee4207
Valentina Ligueno <valentina.ligueno@gmail.com>
2018-11-10 05:10:26 -0300

    bacanudo

--- proba.c
+++ proba.c
@@ -4,6 +4,9 @@
 #include <ctype.h>
 
 int NUMERO_PERMUTACIONES;
+char *NOMBRE_ARCHIVO;
+int** MATRIZ_ADYACENCIA
+
 
 void liberarArreglo1(char* arreglo)
 {
@@ -134,8 +137,105 @@ char** permutaciones(char *lista)
 	}	
 }
 
+void recibirNombreArchivo() 
+{ //Esta función es la que se encarga de pedirle al usuario el nombre de cada uno de los archivos de entrada
+	FILE* arch; //Almacenandolos en NOMBRE_ARCHIVO_1 y NOMBRE_ARCHIVO_2 respectivamente, siendo
+	NOMBRE_ARCHIVO = (char*)malloc(sizeof(char)*25);//Estas variables globales definidas en las definiciones
+	printf("Para comenzar primero se necesita el nombre de sus dos archivos de entrada junto a su formato\n");
+	printf("Por ejemplo 'entrada1.txt' o prueba1.txt\n\nRecuerde que el primero es el que contiene las instrucciones y el segundo las lineas de control\n");
+	do
+	{
+		printf("\nIngrese el nombre del primer archivo solicitado: ");
+		scanf("%s",NOMBRE_ARCHIVO);
+		while(getchar()!='\n');
+		arch = fopen(NOMBRE_ARCHIVO,"r");
+		
+		if (arch == NULL) 
+			printf("No se encuentra archivo con ese nombre, intente nuevamente\n");
+		
+	} while (arch == NULL);
+	fclose(arch);
+}
+
+
+void leerArchivosYGuardarDatos()		//Esta funcion se encarga del proceso de lectura y obtencion de los datos del archivo
+{										//primero cuenta cuantas lineas tiene el archivo	
+	FILE* archivo_nodos;		//hecho esto, pasa todas las instrucciones que están en el archivo
+	int nodo1, nodo2, costo, aux2, i, largo;	//y los registros o direcciones de memoria involucrados en ella
+	char buffer[100],buffer2[100],*valor1,*valor2, *valor3;
+										//La función no retorna nada ya que todo lo que va modificando son punteros
+	archivo_nodos = fopen(NOMBRE_ARCHIVO,"r");
+	fgets(buffer,3,archivo_nodos);
+	
+	int nodos = atoi(buffer);
+	printf("NODOS: %i\n",nodos);
+	MATRIZ_ADYACENCIA = (int**)malloc(sizeof(int*)*nodos)
+	
+	for (int i = 0; i < nodos; ++i)
+	{
+		MATRIZ_ADYACENCIA[i] = (int*)malloc(sizeof(int)*nodos);
+
+		for (int j = 0; j < nodos; ++j)
+			MATRIZ_ADYACENCIA[i][j] = 0;
+	}
+
+	while (!feof(archivo_nodos))
+	{
+		memset(buffer,0, 100);
+		memset(buffer2,0, 100);
+		aux2 = 1;
+		fscanf(archivo_nodos," %119[^\n]",buffer);
+		largo = strlen(buffer);
+
+		for (i = 0; i < largo; ++i)
+			if (buffer[i] == ' ')
+				aux2++;
+
+		if (buffer[0] != '\0' && aux2 != largo && buffer[0] != '\n'&& buffer[0])
+		{	
+			for (i = 0; i < largo; ++i)
+				if (buffer[i] != ' ')
+					break;
+
+			strncpy(buffer2,buffer+i,largo-i);
+	
+			valor1 = strtok(buffer2," ");
+			nodo1 = atoi(valor1)-1;
+
+			valor2 = strtok(NULL," ");
+			nodo2 = atoi(valor2)-1;
+
+			valor3 = strtok(NULL," ");
+			costo = atoi(valor3);
+			printf("%s %s %s\n",valor1,valor2,valor3);
+
+			MATRIZ_ADYACENCIA[nodo1][nodo2] = costo;
+			MATRIZ_ADYACENCIA[nodo2][nodo1] = costo;
+
+		}	
+		fgetc(archivo_nodos);
+		if (feof(archivo_nodos))
+			break;	
+	}
+	fclose(archivo_nodos);
+	for (int i = 0; i < nodos; ++i)
+	{
+		for (int j = 0; j < nodos; ++j)
+		{
+			printf(" |%i|  ",MATRIZ_ADYACENCIA[i][j]);
+		}
+		printf("\n");	
+	}
+}
+
+
 int main(int argc, char const *argv[])
 {
+	recibirNombreArchivo();
+	leerArchivosYGuardarDatos();
+
+	liberarArreglo1(NOMBRE_ARCHIVO);
+
 	char* xd, **xd2;
 	xd = (char*)malloc(sizeof(char)*4);
 	strcpy(xd,"1234");
@@ -145,10 +245,8 @@ int main(int argc, char const *argv[])
 
 	for (int i = 0; i < NUMERO_PERMUTACIONES; ++i)
 		printf(" [%s] ",xd2[i]);
-
 	printf("\n");
-	
+
 	liberarArreglo1(xd);
 	liberarArreglo2(xd2,NUMERO_PERMUTACIONES);
-	
 }
